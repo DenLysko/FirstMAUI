239db70724de6dc09e2105224395c6b465f07294
DenLysko <denislysko@yandex.ru>
2023-09-03 21:52:18 +0300

    feat: Created my empty MAUI application

---
+++ FirstMAUI/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Maui.Controls;
+using System.Diagnostics;
+
+namespace FirstMAUI
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+            string result = await webView.EvaluateJavaScriptAsync($"document.getElementsByClassName(" +
+                $"'mectrl_profilepic mectrl_glyph mectrl_signIn_circle_glyph')[0].innerHTML");
+            Debug.WriteLine(result);
+
+            webView.Eval($"document.getElementsByClassName('mectrl_profilepic mectrl_glyph mectrl_signIn_circle_glyph')[0].click()");
+            
+            
+            
+            
+            
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            // Не совсем понял зачем это нужно
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+    }
+}
